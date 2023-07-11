25dae8ce5665af4c72b6c086c620b86459a0bac6
NikitaFilipov <131530679+NikitaFilipov@users.noreply.github.com>
2023-07-11 22:17:14 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,2 +1,50 @@
-const elem =document.querySelector('#third-right-bl')
-console.log(elem)
+let input = document.querySelector('#field');
+let list = document.querySelector('#list');
+
+input.addEventListener('keypress', function(event) {
+	if (event.key == 'Enter') {
+		// здесь будет код для добавление новой li в список
+       
+        let createLi= () =>{
+            let li = document.createElement('li');
+		
+            let task = document.createElement('span');
+            task.classList.add('task');
+            task.textContent = this.value;
+            li.appendChild(task);
+            task.addEventListener('dblclick',function(){
+                let text=this.textContent
+                this.textContent=''
+
+                let elem =document.createElement('input')
+                elem.value = text
+                this.appendChild(elem)
+                let self=this
+                elem.addEventListener('keypress',function(e){
+                    if(e.key == 'Enter'){
+                        self.textContent=this.value
+                    }
+                })
+            })
+            let remove = document.createElement('span');
+            remove.classList.add('remove');
+            remove.textContent = 'удалить';
+            li.appendChild(remove);
+            remove.addEventListener('click',function(){
+                this.parentElement.remove()
+            })
+            let mark = document.createElement('span');
+            mark.classList.add('mark');
+            mark.textContent = 'сделано';
+            li.appendChild(mark);
+            mark.addEventListener('click',function(){
+                task.classList.add('checked')
+            })
+
+            list.appendChild(li);
+            
+            this.value = '';
+        }
+        createLi()
+    }
+});
